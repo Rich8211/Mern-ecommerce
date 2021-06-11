8d60e325db57536741d1ae5b4dd95796248b6ef2
Rich8211 <richlee821@gmail.com>
2021-06-10 20:15:41 -0400

    Add some conditional rendering for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React, {useContext} from 'react'
+import { useSelector } from 'react-redux'
 import { ModalContext } from '../context/ModalContext'
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const user = useSelector(state => state.user);
+
+    const {userInfo} = user;
+
 
     const { setModal } = useContext(ModalContext);
 
@@ -19,8 +25,13 @@ const Navbar = () => {
         <div className='navbar'>
             <div className='navbar-account-items'>
                 <i className='far fa-user'></i>
-                <p onClick={handleLoginModal}>Sign In</p>
-                <p onClick={handleRegistrationModal}>Register</p>
+                {userInfo ? <p>Sign Out</p> : 
+                <>
+                    <p onClick={handleLoginModal}>Sign In</p>
+                    <p onClick={handleRegistrationModal}>Register</p>
+                </>
+                }
+                
                 <p>Cart</p>
             </div>
             
